0f0edc2a006ebba0fcfc8cf83ad0dae11f4bdb28
adobrova <rezolventa86@rambler.ru>
2024-07-04 23:26:40 +0500

    Added the lastname

--- src/main/java/ru/rambler/rezolventa86/Main.java
+++ src/main/java/ru/rambler/rezolventa86/Main.java
@@ -2,6 +2,6 @@ package ru.rambler.rezolventa86;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! I am Albina");
+        System.out.println("Hello world! I am Albina Dobrova!!!");
     }
 }
